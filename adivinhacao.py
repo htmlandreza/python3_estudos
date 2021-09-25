b3165f5b7d077a9fa17234fdc458d1ac2024944d
Andreza Moreira <htmlandreza@gmail.com>
2021-09-25 18:33:18 -0300

    Definindo pontuação ao jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,12 +7,12 @@ print("*********************************")
 numero_secreto = random.randrange(1, 101)
 total_tentativas = 0
 #print(numero_secreto)
+pontos = 1000
 
 print("Qual nível de dificuldade?")
 print("(1) Fácil (2) Médio (3) Difícil")
 
 nivel = int(input("Defina um nível:"))
-
 if (nivel == 1):
     total_tentativas = 20
 elif (nivel == 2):
@@ -38,14 +38,16 @@ for rodada in range(1, total_tentativas + 1):
     menor = chute < numero_secreto
 
     if(acertou):
-        print("Você acertou!")
+        print("Você acertou e fez {} pontos!".format(pontos))
         break
     else:
         if(maior):
             print("Você errou! Seu chute foi maior que o número secreto.", end="\n\n")
         elif(menor):
             print("Você errou! Seu chute foi menor que o número secreto.", end="\n\n")
-
+        # número absoluto
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 print("Fim do Jogo!")
 
 
