6f064db77e1c31bef6af97522e4c7a05d10042c9
Andreza Moreira <htmlandreza@gmail.com>
2021-09-27 01:25:19 -0300

    Formação de string com funções

--- forca.py
+++ forca.py
@@ -8,6 +8,15 @@ def jogar():
     acertou = False
 
     while(not enforcou and not acertou):
+        index = 0
+        chute = input("Qual letra?")
+        chute = chute.strip()
+
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
         print("jogando ...")
 
     print("Fim do Jogo!")
